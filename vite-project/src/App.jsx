174a2e03d400174dd214012e3c40e2028566e286
Daksh2356 <dakshmakhija@gmail.com>
2024-05-08 23:58:23 +0530

    navbar and footer updated

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -50,48 +50,80 @@ function App() {
 
   return (
     <>
-      <div className="absolute h-[100px] w-full bg-sky-500" >
-        <img className="m-auto h-full p-2 rounded-full" src="../src/assets/logo.jpg"/>
-        </div>
-      <div className="flex flex-col gap-16 items-center justify-center h-screen text-2xl">
-        <h1 className="pt-5 text-3xl">
-          Multiple Usecases of Decentralized Storage Protocols
-        </h1>
-        <div className="p-3 border-2 border-blue-700 rounded-lg ">
-          <label htmlFor="ipfsFile" className="form-label">
-            Choose File :{" "}
-          </label>
-          <input
-            type="file"
-            onChange={changeHandler}
-            multiple
-            id="ipfsFile"
-            accept=".jpg, .jpeg, .png, .gif"
+      <div className="flex flex-col h-screen">
+        <div className="h-16 w-full bg-sky-500 flex justify-between">
+          <img
+            className="ml-4 h-full p-2 rounded-full"
+            src="../src/assets/gtbit_logo.jpg"
+            alt="logo"
           />
-          <button
-            className="border border-solid py-3 px-2.5 rounded w-32 text-base font-medium bg-gray-500 border-transparent cursor-pointer transition text-xl"
-            onClick={handleSubmission}
-          >
-            Submit
-          </button>
+          <img
+            className="ml-4 h-full p-4 rounded-full cursor-pointer"
+            src="../src/assets/github_icon.png"
+            alt="github"
+          />
         </div>
-
-        {cid !== null && (
-          <div className="p-3 border-2 border-red-700 rounded-lg text-base">
-            <img
-              src={`${import.meta.env.VITE_GATEWAY_URL}/ipfs/${cid}`}
-              alt="ipfs image"
-              className="max-h-80 w-auto"
+        <div className="flex flex-col gap-16 flex-grow items-center justify-center text-2xl">
+          <h1 className="md:flex-col pt-5 text-3xl font-bold">
+            Multiple Usecases of Decentralized Storage Protocols
+          </h1>
+          <div className="p-3 border-2 border-blue-700 rounded-lg ">
+            <label htmlFor="ipfsFile" className="form-label">
+              Choose File :{" "}
+            </label>
+            <input
+              type="file"
+              onChange={changeHandler}
+              multiple
+              id="ipfsFile"
+              accept=".jpg, .jpeg, .png, .gif"
             />
-            <div className="p-1 text-center">Your IPFS hash: {cid} </div>
-            <div className="p-1 text-center">
-              {" "}
-              <a href={`${import.meta.env.VITE_GATEWAY_URL}/ipfs/${cid}`}>
-                Click here to see the uploaded image!!
-              </a>{" "}
-            </div>
+            <button
+              className="border border-solid py-3 px-2.5 rounded w-32 text-base font-medium bg-gray-500 border-transparent cursor-pointer transition text-xl mt-3 "
+              onClick={handleSubmission}
+            >
+              Submit
+            </button>
           </div>
-        )}
+
+          {cid !== null && (
+            <div className="p-3 border-2 border-red-700 rounded-lg text-base">
+              <img
+                src={`${import.meta.env.VITE_GATEWAY_URL}/ipfs/${cid}`}
+                alt="ipfs image"
+                className="max-h-80 w-auto"
+              />
+              <div className="p-1 text-center">Your IPFS hash: {cid} </div>
+              <div className="p-1 text-center">
+                {" "}
+                <a
+                  className=""
+                  href={`${import.meta.env.VITE_GATEWAY_URL}/ipfs/${cid}`}
+                >
+                  Click here to see the uploaded image!!
+                </a>{" "}
+              </div>
+            </div>
+          )}
+        </div>
+        <div className="h-16 bg-sky-500 flex justify-between">
+        <img
+            className="ml-4 h-full p-2 rounded-full"
+            src="../src/assets/gtbit_logo.jpg"
+            alt="logo"
+          />
+          <div className="m-auto flex gap-24 font-semibold">
+          <p>Anjni Srivastava</p>
+          <p>Anmol Gandhi</p>
+          <p>Daksh Makhija</p>
+          <p>Kanishk Khurana</p>
+          </div>
+           <img
+            className="ml-4 h-full p-4 rounded-full cursor-pointer"
+            src="../src/assets/github_icon.png"
+            alt="github"
+          />
+        </div>
       </div>
     </>
   );
